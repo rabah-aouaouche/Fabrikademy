816902842d4e8da6acf7c7508b2cf30b22536fc2
rabah-aouaouche <rabehaou@gmail.com>
2022-12-06 15:57:33 +0100

    myproject

---
+++ program.py
@@ -0,0 +1,10 @@
+nomClient= "test"
+x=int(input("Tapez le premier numero "))
+y=int(input("Tapez le deuxieme numero "))
+z=int(input("Tapez le troisiéme numero "))
+print(" la somme de " , x , " - " ,  y , "-" , z ," = " ,x-y-z)
+
+prenom=(input("Tapez votre Prénom: "))
+age=(input("Donnez votre age: "))
+adresse=(input("Votre adresse: "))
+print("vos informations personnels: ", prenom,age,adresse, sep="\n")
